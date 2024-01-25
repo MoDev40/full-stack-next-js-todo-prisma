aadf23ecfabf1ac6d527045cf96df2b125d5867e
M40 <modev.404@gmailcom>
2024-01-25 16:56:00 +0300

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import TodoFormDialog from "./_component/todo/TodoFormDialog";
 import Todos from "./_component/todo/Todos";
 
+export const dynamic = 'force-dynamic';
+
 export default function Home() {
   return (
     <div className="max-w-[1200px] mx-auto p-6 space-y-4">
