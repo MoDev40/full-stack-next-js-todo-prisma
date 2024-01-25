481c7741eeb4a403f179e61fa53ecd2c383ae1af
M40 <modev.404@gmailcom>
2024-01-25 16:59:23 +0300

    updated deployment url

--- lib/config.ts
+++ lib/config.ts
@@ -1,4 +1,4 @@
 export const sekret = process.env.SEKRET_KEY as string
-export const API_URL = process.env.NODE_ENV === 'production' ? ''
+export const API_URL = process.env.NODE_ENV === 'production' ? 'full-stack-next-js-todo-prisma.vercel.app/api'
     : 'http://localhost:3000/api';
 
